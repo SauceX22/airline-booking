e2663297430683dfdc6dbb3fb19477cc2b9ae0e8
SauceX <mahmoudzak4@gmail.com>
2024-04-24 01:24:00 +0300

    refactor: change is manager terminology

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -4,6 +4,7 @@ import { Icons } from "@/components/icons";
 import { buttonVariants } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
+import { auth } from "@/server/auth";
 
 import { type Metadata } from "next";
 import Link from "next/link";
@@ -13,14 +14,14 @@ export const metadata: Metadata = {
 };
 
 export default async function HomePage() {
-  // const session = await auth();
-  // const isManager = session?.user.role === "ADMIN";
+  const session = await auth();
+  const isAdmin = session?.user.role === "ADMIN";
 
   return (
     <DashboardShell>
       <DashboardHeader heading="Home" text="Bikes available for rent.">
         <Link prefetch href="#" className={cn(buttonVariants())}>
-          {/* {isManager ? "Manage User Reservations" : "View Your Reservations"} */}
+          {isAdmin ? "Manage User Reservations" : "View Your Reservations"}
         </Link>
       </DashboardHeader>
       <div className="px-2">test</div>
