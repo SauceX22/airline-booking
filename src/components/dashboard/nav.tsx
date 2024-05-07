4e37ed2b2c846363da2d56e414b911ccd2571b47
SauceX <mahmoudzak4@gmail.com>
2024-05-08 00:51:25 +0300

    fix: make navbar server component

--- src/components/dashboard/nav.tsx
+++ src/components/dashboard/nav.tsx
@@ -1,20 +1,28 @@
-"use client";
-
+import { headers } from "next/headers";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 import { Icons } from "@/components/icons";
 import { cn } from "@/lib/utils";
+import { auth } from "@/server/auth";
 import { type SidebarNavItem } from "@/types";
-import { useSession } from "next-auth/react";
 
 interface DashboardNavProps {
   items: SidebarNavItem[];
 }
 
-export function DashboardNav({ items }: DashboardNavProps) {
-  const path = usePathname();
-  const { data: session } = useSession();
+export async function DashboardNav({ items }: DashboardNavProps) {
+  const headersList = headers();
+  // host: 'localhost:3000',
+  const domain = headersList.get("host") ?? "";
+  // referer: 'http://localhost:3000/home',
+  const fullUrl = headersList.get("referer") ?? "";
+
+  const path = fullUrl
+    .replace(domain, "")
+    .replace("http://", "")
+    .replace("https://", "");
+
+  const session = await auth();
 
   if (!items?.length) {
     return null;
@@ -23,7 +31,7 @@ export function DashboardNav({ items }: DashboardNavProps) {
   return (
     <nav className="grid items-start gap-1">
       {items.map((item, index) => {
-        if (item.managerOnly && session?.user.role !== "ADMIN") {
+        if (item.adminOnly && session?.user.role !== "ADMIN") {
           return null;
         }
 
@@ -34,10 +42,12 @@ export function DashboardNav({ items }: DashboardNavProps) {
               <span
                 className={cn(
                   "group flex items-center rounded-md px-3 py-3 text-base font-medium hover:bg-accent hover:text-accent-foreground",
+                  session?.user.role === "ADMIN" && item.adminOnly
+                    ? "border border-dashed border-orange-500"
+                    : "",
                   path === item.href ? "bg-accent" : "transparent",
-                  item.disabled && "cursor-not-allowed opacity-80",
-                )}
-              >
+                  item.disabled && "cursor-not-allowed opacity-80"
+                )}>
                 <Icon className="mr-2 h-4 w-4" />
                 <span>{item.title}</span>
               </span>
