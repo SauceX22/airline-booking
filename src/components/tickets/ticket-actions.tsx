5f830b9a3b33f1d68f61278966be5a8760c3a72f
SauceX <mahmoudzak4@gmail.com>
2024-05-09 21:22:18 +0300

    fix: hide payment for admin

--- src/components/tickets/ticket-actions.tsx
+++ src/components/tickets/ticket-actions.tsx
@@ -3,32 +3,15 @@ import React from "react";
 import "next/navigation";
 
 import Link from "next/link";
-import {
-  type CreditCard,
-  type Flight,
-  type Plane,
-  type Ticket,
-} from "@prisma/client";
-import { ArrowLeftIcon, DeleteIcon, DollarSignIcon, XIcon } from "lucide-react";
-import { toast } from "sonner";
+import { type Flight, type Plane, type Ticket } from "@prisma/client";
+import { ArrowLeftIcon } from "lucide-react";
 
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { pushModal } from "@/components/modals";
+import { buttonVariants } from "@/components/ui/button";
 import EditTicketDialogButton from "@/components/modals/edit-ticket-dialog";
 import TicketCancelDialogButton from "@/components/tickets/ticket-cancel-button";
 import TicketPaymentSheetButton from "@/components/tickets/ticket-payment-sheet-button";
 import { cn } from "@/lib/utils";
+import { auth } from "@/server/auth";
 import { api } from "@/trpc/server";
 
 interface TicketActionsProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -39,8 +22,10 @@ export async function TicketItemActions({
   ticket,
   className,
 }: TicketActionsProps) {
+  const session = await auth();
+  const isAdmin = session?.user.role === "ADMIN";
   const creditCards = await api.creditCard.getAll.query();
-  const existingUserTickets = await api.ticket.getUserFlightTickets.query({
+  const existingUserTickets = await api.ticket.getUserTickets.query({
     flightId: ticket.flightId,
   });
 
@@ -56,7 +41,9 @@ export async function TicketItemActions({
         existingUserTickets={existingUserTickets}
       />
       <TicketCancelDialogButton ticket={ticket} />
-      <TicketPaymentSheetButton cards={creditCards} ticket={ticket} />
+      {!isAdmin && (
+        <TicketPaymentSheetButton cards={creditCards} ticket={ticket} />
+      )}
     </div>
   );
 }
