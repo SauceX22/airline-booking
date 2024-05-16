51777175d15ed02bfe34d3e05ee5336b7bf8bf38
SauceX <mahmoudzak4@gmail.com>
2024-05-16 21:59:46 +0300

    fix: update app url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,6 +9,8 @@ export function cn(...inputs: ClassValue[]) {
 export function getBaseUrl() {
   if (typeof window !== "undefined") return "";
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.APP_URL) return process.env.APP_URL;
+  if (process.env.NEXT_PUBLIC_APP_URL) return process.env.NEXT_PUBLIC_APP_URL;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
